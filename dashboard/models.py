cf5e5a935a74a6a57892009b6b8d538320822c6d
Ajesh Sen Thapa <aj3sshh@gmail.com>
2021-06-08 15:52:47 +0545

    user model removed

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,8 @@
 from django.apps import apps
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+
+
 class DateTimeModel(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True,
